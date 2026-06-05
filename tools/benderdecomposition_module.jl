e6a858930bba556fdb1802eb8c1e788a104f3135
YipingYUAN <45745675+Divas1234@users.noreply.github.com>
2026-06-05 13:37:22 +0800

    Improve Benders cut generation efficiency

--- tools/benderdecomposition_module.jl
+++ tools/benderdecomposition_module.jl
@@ -108,17 +108,9 @@ function multiple_bender_decomposition_scuc(
 		end
 
 		if config_param.is_ConsiderMultiCUTs == 1
-			candidate_cuts = []
-			for (s, ret) in ret_dic
-				if ret.is_feasible == true
-					scenario_recourse_cut = get_reduced_cost_optimality_cut_expression(scuc_masterproblem, ret, iter_value)
-					cut_value = value(scenario_recourse_cut)
-					theta_value = value(scuc_masterproblem[:θ][s])
-					push!(candidate_cuts, (cut_value - theta_value, s, scenario_recourse_cut))
-				end
-			end
+			candidate_cuts = collect_scenario_cut_candidates(ret_dic, value.(scuc_masterproblem[:θ]))
 			add_jensen_cut_if_violated!(scuc_masterproblem, jensen_subproblem_struct, iter_value, NG, NT)
-			add_violated_scenario_optimality_cuts!(scuc_masterproblem, candidate_cuts)
+			add_selected_scenario_optimality_cuts!(scuc_masterproblem, candidate_cuts, ret_dic, iter_value)
 			if !all_subproblems_feasibility_flag
 				add_no_good_cut!(scuc_masterproblem, x⁽⁰⁾, u⁽⁰⁾, v⁽⁰⁾)
 			end
@@ -134,6 +126,67 @@ function multiple_bender_decomposition_scuc(
 			end
 		end
 	end
+end
+
+function collect_scenario_cut_candidates(
+	ret_dic::OrderedDict{Int64, Any},
+	theta_values;
+	tolerance::Float64 = parse(Float64, get(ENV, "BENDERS_CUT_VIOLATION_TOL", "1e-5")),
+)
+	scenario_ids = collect(keys(ret_dic))
+	candidate_slots = Vector{Union{Nothing, Tuple{Float64, Int64}}}(nothing, length(scenario_ids))
+	if should_build_cut_candidates_in_parallel(length(scenario_ids))
+		Threads.@threads for idx in eachindex(scenario_ids)
+			s = scenario_ids[idx]
+			ret = ret_dic[s]
+			if ret.is_feasible == true
+				violation = Float64(ret.θ - theta_values[s])
+				if violation > tolerance
+					candidate_slots[idx] = (violation, s)
+				end
+			end
+		end
+	else
+		for idx in eachindex(scenario_ids)
+			s = scenario_ids[idx]
+			ret = ret_dic[s]
+			if ret.is_feasible == true
+				violation = Float64(ret.θ - theta_values[s])
+				if violation > tolerance
+					candidate_slots[idx] = (violation, s)
+				end
+			end
+		end
+	end
+
+	candidate_cuts = Tuple{Float64, Int64}[]
+	for candidate in candidate_slots
+		candidate === nothing && continue
+		push!(candidate_cuts, candidate)
+	end
+	sort!(candidate_cuts; by = cut -> cut[1], rev = true)
+	max_cuts = max(0, parse(Int64, get(ENV, "BENDERS_MAX_SCENARIO_CUTS_PER_ITERATION", string(length(candidate_cuts)))))
+	if max_cuts < length(candidate_cuts)
+		resize!(candidate_cuts, max_cuts)
+	end
+	return candidate_cuts
+end
+
+function should_build_cut_candidates_in_parallel(candidate_count::Int64)
+	return candidate_count > 1 && Threads.nthreads() > 1 && get(ENV, "BENDERS_PARALLEL_CUT_CANDIDATES", "1") != "0"
+end
+
+function add_selected_scenario_optimality_cuts!(model::Model, selected_candidates, ret_dic, iter_value)
+	added = 0
+	for (_, s) in selected_candidates
+		cut_expr = get_reduced_cost_optimality_cut_expression(model, ret_dic[s], iter_value)
+		@constraint(model, model[:θ][s] >= cut_expr)
+		added += 1
+	end
+	if get(ENV, "BENDERS_VERBOSE_CUTS", "0") == "1"
+		println("Added scenario optimality cuts: ", added)
+	end
+	return added
 end
 
 function add_violated_scenario_optimality_cuts!(model::Model, candidate_cuts; tolerance::Float64 = parse(Float64, get(ENV, "BENDERS_CUT_VIOLATION_TOL", "1e-5")))
@@ -298,6 +351,41 @@ function should_solve_subproblems_in_parallel(NS::Int64)
 	return NS > 1 && Threads.nthreads() > 1 && get(ENV, "BENDERS_PARALLEL_SUBPROBLEMS", "1") != "0"
 end
 
+function should_collect_dual_coefficients()
+	return get(ENV, "BENDERS_COLLECT_DUAL_COEFFS", "0") == "1"
+end
+
+function should_collect_dual_details()
+	return get(ENV, "BENDERS_COLLECT_DUAL_DETAILS", "0") == "1"
+end
+
+function maybe_get_dual_coefficients(scuc_subproblem_dic::SCUC_Model, opti_termination_status::Bool, NT::Int64, NG::Int64)
+	if !should_collect_dual_coefficients()
+		return Dict{Symbol, dual_subprob_expr_coefficient}()
+	end
+	constraints = scuc_subproblem_dic.reformated_constraints
+	res_smaller_than = get_dual_constrs_coefficient(scuc_subproblem_dic, constraints._smaller_than, opti_termination_status, NT, NG)
+	res_equal_to = get_dual_constrs_coefficient(scuc_subproblem_dic, constraints._equal_to, opti_termination_status, NT, NG)
+	res_greater_than = get_dual_constrs_coefficient(scuc_subproblem_dic, constraints._greater_than, opti_termination_status, NT, NG)
+	return merge(res_equal_to, res_smaller_than, res_greater_than)
+end
+
+function maybe_get_dual_detail_dictionaries(scuc_subproblem_dic::SCUC_Model, opti_termination_status::Bool)
+	if !should_collect_dual_details()
+		return (
+			smaller = Dict{Symbol, Any}(),
+			greater = Dict{Symbol, Any}(),
+			equal = Dict{Symbol, Any}(),
+		)
+	end
+	dual_getter = opti_termination_status ? dual : shadow_price
+	return (
+		smaller = Dict(k => dual_getter.(v) for (k, v) in scuc_subproblem_dic.reformated_constraints._smaller_than),
+		greater = Dict(k => dual_getter.(v) for (k, v) in scuc_subproblem_dic.reformated_constraints._greater_than),
+		equal = Dict(k => dual_getter.(v) for (k, v) in scuc_subproblem_dic.reformated_constraints._equal_to),
+	)
+end
+
 """
 `solve_subproblem_with_feasibility_cut(...)`
 
@@ -339,12 +427,8 @@ function solve_subproblem_with_feasibility_cut(scuc_subproblem_dic::SCUC_Model, 
 	# res_greater_than = get_dual_constrs_coefficient(
 	# 	scuc_subproblem_dic, constrs_greater_than, opti_termination_status)
 
-	constraints = scuc_subproblem_dic.reformated_constraints
-	res_smaller_than = get_dual_constrs_coefficient(scuc_subproblem_dic, constraints._smaller_than, opti_termination_status, NT, NG)
-	res_equal_to = get_dual_constrs_coefficient(scuc_subproblem_dic, constraints._equal_to, opti_termination_status, NT, NG)
-	res_greater_than = get_dual_constrs_coefficient(scuc_subproblem_dic, constraints._greater_than, opti_termination_status, NT, NG)
-
-	final_dual_subproblem_coefficient_results = merge(res_equal_to, res_smaller_than, res_greater_than)
+	final_dual_subproblem_coefficient_results = maybe_get_dual_coefficients(scuc_subproblem_dic, opti_termination_status, NT, NG)
+	dual_details = maybe_get_dual_detail_dictionaries(scuc_subproblem_dic, opti_termination_status)
 
 	if opti_termination_status == true
 		# Return solution information with scaled duals for numerical stability
@@ -360,9 +444,9 @@ function solve_subproblem_with_feasibility_cut(scuc_subproblem_dic::SCUC_Model, 
 			dual_coeffs = final_dual_subproblem_coefficient_results,
 
 			# NOTE - additional dual info
-			dual_smaller_than_constr_dic = Dict(k => dual.(v) for (k, v) in scuc_subproblem_dic.reformated_constraints._smaller_than),
-			dual_greater_than_constr_dic = Dict(k => dual.(v) for (k, v) in scuc_subproblem_dic.reformated_constraints._greater_than),
-			dual_equal_to_constr_dic = Dict(k => dual.(v) for (k, v) in scuc_subproblem_dic.reformated_constraints._equal_to),
+			dual_smaller_than_constr_dic = dual_details.smaller,
+			dual_greater_than_constr_dic = dual_details.greater,
+			dual_equal_to_constr_dic = dual_details.equal,
 		)
 	else
 		# Get Farkas certificate (dual rays) for infeasibility
@@ -380,9 +464,9 @@ function solve_subproblem_with_feasibility_cut(scuc_subproblem_dic::SCUC_Model, 
 			dual_coeffs = final_dual_subproblem_coefficient_results,
 
 			# NOTE - additional dual info
-			dual_smaller_than_constr_dic = Dict(k => shadow_price.(v) for (k, v) in scuc_subproblem_dic.reformated_constraints._smaller_than),
-			dual_greater_than_constr_dic = Dict(k => shadow_price.(v) for (k, v) in scuc_subproblem_dic.reformated_constraints._greater_than),
-			dual_equal_to_constr_dic = Dict(k => shadow_price.(v) for (k, v) in scuc_subproblem_dic.reformated_constraints._equal_to),
+			dual_smaller_than_constr_dic = dual_details.smaller,
+			dual_greater_than_constr_dic = dual_details.greater,
+			dual_equal_to_constr_dic = dual_details.equal,
 		)
 	end
 end
